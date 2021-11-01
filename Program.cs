9f316d5a305007baf810aa66e14bafa6e4db6337
thiwaK <thiwanka.kaushal.mob@gmail.com>
2021-11-01 15:20:05 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Thiwanka Kaushal
+ * Date: 11/1/2021
+ * Time: 3:19 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Cryptor
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
