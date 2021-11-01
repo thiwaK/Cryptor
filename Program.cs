0a4811baa5348e0608245855f8724923edc278f0
thiwaK <thiwanka.kaushal.mob@gmail.com>
2021-11-01 16:01:27 +0530

    basic args check add

--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@
  * User: Thiwanka Kaushal
  * Date: 11/1/2021
  * Time: 3:19 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 
@@ -12,12 +10,61 @@ namespace Cryptor
 {
 	class Program
 	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
+		public const string VERSION = "1.0";
+		public const string DATE = "1 Nov 2021";
+		static bool includeSubfolders = false;
+		
+		const string ERROR_INVALID_OPTION = @"
+	[-] This is not a valid entry. Press H to HELP
+	[!] Press A to see ABOUT
+		";
+		
+		public static void Main(string[] args){
+				
+			string method = @"";
+			string path = @"";
 			
-			// TODO: Implement Functionality Here
+			if (args.Length <= 1){
+//				log.Error(ERROR_MSG, "  |  Input Error", false);
+				ConsoleKeyInfo cki = Console.ReadKey();
+				
+				switch(cki.KeyChar.ToString().ToUpper()){
+				
+					case "H":
+						break;
+						
+					case "A":
+						break;
+					
+					default:
+//						log.Exit();
+						break;
+				}
+
+
+			}
 			
+			foreach (var item in args){
+				
+				switch(item.ToUpper()){
+				
+					case "-H":
+						break;
+						
+					case "--HELP":
+						break;
+					
+					case "-S":
+						break;
+						
+				
+				}
+				
+			}
+
+
+			Console.WriteLine(ERROR_INVALID_OPTION);
+
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
